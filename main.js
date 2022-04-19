b842b8aaa0a0d2ec28f624d2910208096596b076
sandro <92390693+sandro-olgesashvili@users.noreply.github.com>
2022-04-19 15:11:20 +0400

    add done

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const SEARCHAPI =
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
-const searhc = document.getElementById('search')
+const search = document.getElementById('search')
 
-getMovies() 
+getMovies(APIURL) 
 
 
-async function getMovies() {
-    const res = await fetch(APIURL);
+async function getMovies(url) {
+    const res = await fetch(url);
 
     const data = await res.json()
 
@@ -29,20 +29,27 @@ async function getMovies() {
 function showMovies (movies) {
     main.innerHTML = '';
     movies.forEach(movie => {
-        const {poster_path, original_title, vote_average } = movie
+        if(movie.poster_path !== null ) {
 
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie')
+            const {poster_path, original_title, vote_average, overview } = movie
+    
+            const movieEl = document.createElement('div');
+            movieEl.classList.add('movie')
+    
+            movieEl.innerHTML = `
+                <img src="${IMGPATH + poster_path}" alt="${original_title}">
+                <div class="movie-info">
+                    <h3>${original_title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                </div>
 
-        movieEl.innerHTML = `
-            <img src="${IMGPATH + poster_path}" alt="${original_title}">
-            <div class="movie-info">
-                <h3>${original_title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-        `
-
-        main.appendChild(movieEl)
+                <div class="overview"><h4>Overview: </h4>
+                <br>                
+                ${overview} </div>
+            `
+    
+            main.appendChild(movieEl)
+        }
     })
 
 }
@@ -61,12 +68,12 @@ function getClassByRate(vote) {
 form.addEventListener('submit', e => {
     e.preventDefault()
     
-    const searchTerm = searhc.value
+    const searchTerm = search.value
 
     if(searchTerm) {
 
-
+        getMovies(SEARCHAPI + searchTerm)
         
-        searhc.value = ''
+        search.value = ''
     }
 })
